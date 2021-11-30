7c9f338d1f147f3a8fb0ccfdb585fba148f13eab
Asharppen <asharppen@gmail.com>
2021-12-01 00:18:58 +0100

    Initial implementation

---
+++ src/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valheim.ThisGoesHere
+{
+    public class Config
+    {
+        public List<ConfigEntry> Entries { get; set; }
+    }
+
+    public class ConfigEntry
+    {
+        public string FromFile { get; set; }
+
+        public string ToFile { get; set; }
+    }
+}
